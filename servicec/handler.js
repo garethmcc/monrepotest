070e1fbd447b55d95e5ae9c14a44598db5d94c92
Gareth Mc Cumskey <gareth@mccumskey.com>
2020-02-03 21:42:26 +0200

    Some more commit

--- servicec/handler.js
+++ servicec/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Changes',
+        message: 'changeagain',
         input: event,
       },
       null,
