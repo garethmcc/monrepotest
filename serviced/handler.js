98dad64b5533cc4251380cc445e1a2600d24c3ac
Gareth Mc Cumskey <gareth@mccumskey.com>
2020-03-26 12:03:38 +0200

    Changes!!!!!

--- serviced/handler.js
+++ serviced/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'HI THERE',
+        message: 'HI THEREagain',
         input: event,
       },
       null,
