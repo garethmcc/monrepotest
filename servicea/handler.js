f486fa378198577622480a7b0c0b96f500ab7662
gareth <gareth@mccumskey.com>
2020-06-09 16:44:49 +0200

    made change

--- servicea/handler.js
+++ servicea/handler.js
@@ -2,9 +2,10 @@
 const fetch = require('node-fetch')
 const bcrypt = require('bcryptjs')
 module.exports.hello = async (event, context) => {
+  console.log('Hello')
     await context.serverlessSdk.span('HASH', async () => {
         return new Promise((resolve, reject) => {
-            bcrypt.hash("Another change", 10, () => {
+            bcrypt.hash("Another change!!!", 10, () => {
                 resolve()
             })
         })
@@ -12,6 +13,11 @@ module.exports.hello = async (event, context) => {
     await context.serverlessSdk.span('Dog Facts', async () => {
         return fetch('https://some-random-api.ml/facts/dog')
     })
+
+
+
+
+
   return {
     statusCode: 200,
     body: JSON.stringify(
