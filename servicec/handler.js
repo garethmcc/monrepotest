656af611a93144b6765932d9e2655152778c23d1
gareth <gareth@mccumskey.com>
2020-07-27 10:07:25 +0200

    Service c change

--- servicec/handler.js
+++ servicec/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'changeagain',
+        message: 'changfeoncemore',
         input: event,
       },
       null,
