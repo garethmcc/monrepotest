9eac1fbb0e314212f11068c565dfa7d538ae8f36
gareth <gareth@mccumskey.com>
2020-10-09 08:16:13 +0200

    Testing deployment notifications

--- servicec/handler.js
+++ servicec/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'changed',
+        message: 'tweak',
         input: event,
       },
       null,
