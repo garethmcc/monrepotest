7e195c93019bfef216b4f64637e1960aff32f793
Gareth McCumskey <gareth@mccumskey.com>
2021-08-02 19:55:29 +0100

    Update handler
    Updated handler

--- servicec/handler.js
+++ servicec/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'change',
+        message: 'change2',
         input: event,
       },
       null,
